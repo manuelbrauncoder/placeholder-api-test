be496b8519230ebbaecb98fd028b5f4d63f4bc2a
Manuel Braun <manuel.braun.coder@gmail.com>
2024-12-01 08:14:32 +0100

    Initial Commit

---
+++ Api-Crud-test/Api_Crud_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_Crud_testApp.swift
+//  Api-Crud-test
+//
+//  Created by Manuel Braun on 01.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Api_Crud_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Api-Crud-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Api-Crud-test
+//
+//  Created by Manuel Braun on 01.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
